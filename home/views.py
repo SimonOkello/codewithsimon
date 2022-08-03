9be9e21ffb977055e3d7cd6d45e1bae7f93dcc6c
SimonOkello <simonokello93@gmail.com>
2022-08-03 06:21:12 +0300

    update blog

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.conf import settings
 import requests
+import httpx
 import json
 from django.contrib import messages
 
@@ -11,15 +12,16 @@ def index(request):
     context = {}
     category_url = f'{settings.CODEWITHSIMON_BASE_URL}/dashboard/'
     try:
-        res = requests.get(category_url)
+        res = httpx.get(category_url)
         if res.status_code == 200:
             try:
                 response = json.loads(res.text)
                 context = {
                     'categories': response['categories'],
-                    'trending_today': response['trending_today'],
+                    'trending_this_week': response['trending_this_week'],
                     'recent_posts': response['recent_posts'],
-                    'beginner_posts': response['beginner_posts'],
+                    'beginner_two_posts': response['beginner_two_posts'],
+                    'beginner_three_posts': response['beginner_three_posts'],
                 }
             except KeyError as error:
                 pass
